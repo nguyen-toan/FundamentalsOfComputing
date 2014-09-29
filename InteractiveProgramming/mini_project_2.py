a26d491d54a0ecb5108ce0cb5e66311d9bcd38c4
Nguyen Toan <nguyentoan1508@gmail.com>
2014-09-30 07:39:02 +0900

    implement remaining guesses

--- InteractiveProgramming/mini_project_2.py
+++ InteractiveProgramming/mini_project_2.py
@@ -3,15 +3,21 @@
 
 import simplegui
 import random
+import math
 
 secret_number = 0
 maxrange = 100
+maxguess = 0
 
 # helper function to start and restart the game
 def new_game():
     global secret_number
     secret_number = random.randrange(0, maxrange)
-    print 'Secret number is set to range: 0 - ' + str(maxrange)
+    global maxguess
+    maxguess = int(math.ceil(math.log(maxrange+1, 2)))
+    print
+    print 'New game starts with secret number set to range: 0 - ' + str(maxrange)
+    print 'Number of remaining guesses is ' + str(maxguess)
 
 # define event handlers for control panel
 def range100():
@@ -27,16 +33,29 @@ def range1000():
     new_game()
     
 def input_guess(guess):
-    print 'Guess was ' + guess
+    
+    # print guess number
+    print
     guess = int(guess)
-    if guess < secret_number:
-        print 'Higher'
-    elif guess > secret_number:
-        print 'Lower'
-    else:
+    print 'Guess was ' + str(guess)
+    
+    # print remaining guesses
+    global maxguess
+    maxguess = maxguess -1
+    print 'Number of remaining guesses is ' + str(maxguess)
+    
+    # compare guess number with secret number
+    if guess == secret_number:
         print 'Correct!'
         new_game()
-
+    else:
+        if maxguess == 0:
+            print 'You ran out of guesses.  The number was ' + str(secret_number)
+            new_game()
+        if guess < secret_number:
+            print 'Higher'
+        else:
+            print 'Lower'
     
 # create frame
 frame = simplegui.create_frame("Guess the number", 300, 300)
@@ -46,7 +65,6 @@ rang1000_button = frame.add_button("Range: 0 - 1000", range1000)
 
 # register event handlers for control elements and start frame
 frame.start()
-
 
 # call new_game 
 new_game()
